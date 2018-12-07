27e8e45979985f804911769f672aca7625f30570
ueliton mendes <carine.wp@hotmail.com>
2018-12-07 15:24:21 -0200

    Começando com a interface, alterei o index.php e o config.php

--- config.php
+++ config.php
@@ -23,4 +23,7 @@ if ( !defined('BASEURL') )
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
     define('DBAPI', ABSPATH . 'repositorio/database.php');
+/** caminhos dos templates de header e footer **/	
+define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');	
+define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
 ?>
